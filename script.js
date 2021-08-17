f65aea702ef9721743c318cc00a63ca3dafdfea1
eagle <eagle.lam.le@Gmail.com>
2021-08-16 21:42:49 -0400

    const

---
+++ script.js
@@ -0,0 +1,21 @@
+let APIKey = 'a197130c61659a67b04b4e96ad8d285e';
+
+
+const searchForm = document.getElementById('searchForm')
+const searchBtn = document.getElementById('searchBtn')
+const searchResults = document.getElementById('searchResults')
+const searchResultsBtn = document.getElementById('searchResultsBtn')
+
+
+const weatherCard = document.getElementById('weatherCard')
+const cityWeather = document.getElementById('cityWeather')
+const tempWeather = document.getElementById('tempWeather')
+const humidityWeather = document.getElementById('humidityWeather')
+const windWeather = document.getElementById('windWeather')
+const UVIndexWeather = document.getElementById('UVIndexWeather')
+const days = document.getElementById('days')
+const eachDay = document.getElementById('eachDay')
+
+const eachDayTemp = document.getElementById('eachDayTemp')
+const eachDayHumidity = document.getElementById('eachDayHumidity')
+const eachDayWind = document.getElementById('eachDayWind')
